52fdf744801f8abdb47da1a3c1ebedf67b413bea
pascalborst <160334873+passqqqqq@users.noreply.github.com>
2025-07-06 22:09:25 +0200

    Update index.ts

--- api/index.ts
+++ api/index.ts
@@ -13,7 +13,24 @@ const urlencodedParser = bodyParser.urlencoded({ extended: false });
 app.use(express.static('public'));
 
 app.get('/', function (req, res) {
-	res.send("Vitamins - The queen provides positive energy, motivation, and enjoyment, expressed as \"vitamins.\" Too much vitamins will give you an \"overdose\", from all her beauty, yapping, and all the cozy moments, leaving you all hyped up and over excited!");
+
+
+    res.send("Lobster - The queen loves to go for sun tanning, but when she tans a little bit too much, her skins becomes red and that makes her look like a lobster!")
+
+
+    // const list = [{
+    //     description: 'Vitamins - The queen provides positive energy, motivation, and enjoyment, expressed as \"vitamins.\" Too much vitamins will give you an \"overdose\", from all her beauty, yapping, and all the cozy moments, leaving you all hyped up and over excited!',
+    //     name: "vitamins",
+    // }, {
+    //     name: "lobstering",
+    //     description: 'Lobster - The queen loves to go for sun tanning, but when she tans a little bit too much, her skins becomes red and that makes her look like a lobster!',
+    // }]
+
+    // const queryParam = req.query.name;
+
+    // const result = list.find(item => item.name === queryParam);
+
+	// res.send(result?.description);
 });
 
 app.listen(8000, () => console.log('Server ready on port 8000.'));
