d9cfd2bc8d3ce604a00c6c8ab1974309caec26a0
pascalborst <160334873+passqqqqq@users.noreply.github.com>
2025-07-06 22:03:37 +0200

    Update index.ts

--- api/index.ts
+++ api/index.ts
@@ -13,7 +13,7 @@ const urlencodedParser = bodyParser.urlencoded({ extended: false });
 app.use(express.static('public'));
 
 app.get('/', function (req, res) {
-	res.send("Vitamins - The queen provides positive energy, motivation, and enjoyment, expressed as \"0vitamins.\" An \"overdose\" humorously refers to watching too much of Karin's beauty, her chatting, and cozy moments, leaving you all hyped up and excited!");
+	res.send("Vitamins - The queen provides positive energy, motivation, and enjoyment, expressed as \"0vitamins.\" Too much vitamins will give you an \"overdose\", from all her beauty, yapping, and all the cozy moments, leaving you all hyped up and over excited!");
 });
 
 app.listen(8000, () => console.log('Server ready on port 8000.'));
