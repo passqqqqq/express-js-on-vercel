4fa3d7b79e3ebb3232fde4da41d19318b202b5e6
pascalborst <160334873+passqqqqq@users.noreply.github.com>
2025-07-06 22:04:04 +0200

    Update index.ts

--- api/index.ts
+++ api/index.ts
@@ -13,7 +13,7 @@ const urlencodedParser = bodyParser.urlencoded({ extended: false });
 app.use(express.static('public'));
 
 app.get('/', function (req, res) {
-	res.send("Vitamins - The queen provides positive energy, motivation, and enjoyment, expressed as \"0vitamins.\" Too much vitamins will give you an \"overdose\", from all her beauty, yapping, and all the cozy moments, leaving you all hyped up and over excited!");
+	res.send("Vitamins - The queen provides positive energy, motivation, and enjoyment, expressed as \"vitamins.\" Too much vitamins will give you an \"overdose\", from all her beauty, yapping, and all the cozy moments, leaving you all hyped up and over excited!");
 });
 
 app.listen(8000, () => console.log('Server ready on port 8000.'));
