5b0fbf5a57bfb5c92ddbab6b9d9a020dbf4990b5
pascalborst <160334873+passqqqqq@users.noreply.github.com>
2025-07-06 22:02:38 +0200

    Update index.ts

--- api/index.ts
+++ api/index.ts
@@ -13,7 +13,7 @@ const urlencodedParser = bodyParser.urlencoded({ extended: false });
 app.use(express.static('public'));
 
 app.get('/', function (req, res) {
-	res.send("The queen provides positive energy, motivation, and enjoyment, expressed as \"0vitamins.\" An \"overdose\" humorously refers to watching too much of Karin's beauty, her chatting, and cozy moments, leaving you all hyped up and excited!");
+	res.send("Vitamins - The queen provides positive energy, motivation, and enjoyment, expressed as \"0vitamins.\" An \"overdose\" humorously refers to watching too much of Karin's beauty, her chatting, and cozy moments, leaving you all hyped up and excited!");
 });
 
 app.listen(8000, () => console.log('Server ready on port 8000.'));
